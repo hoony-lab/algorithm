8361a45a78f9fdc656c1e1c8eedd2882e1fcc80c
Hoony <jihun9838@gmail.com>
2019-09-20 23:13:29 +0900

    190920

--- Algorithm/Algorithm/header.cpp
+++ Algorithm/Algorithm/header.cpp
@@ -7,38 +7,18 @@
 #include <iomanip>
 #include <string>
 #include <math.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define min(a,b) a < b ? a : b;
 #define max(a,b) a > b ? a : b;
+
 using namespace std;
 int main() {
-	//int n;
-	//int min = 1e9;
-	//int max = 0;
-	//cin >> n;
-
-	//int a;
-	//for (int i = 0; i < n; i++)
-	//{
-	//	cin >> a;
-	//	min = min(a, min);
-	//	max = max(a, max);
-	//}
-	//cout << min << " " << max;
-
-	int n, a, min = 1e9, max = -1e9;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> a;
-		if (min > a) min = a;
-		if (max < a) max = a;
-	}
-	cout << min << " " << max;
 
 	system("pause");
 }
-	//vector<vector<int>> v;
-	//sort(v.begin(), v.end(), [](int a, int b) {
 
-	//});
+
 
---
+++ Algorithm/Algorithm/다리를 지나는 트럭.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+	int time = 0;
+	int total_weight = 0;
+	int ind = 0;
+
+	// 무게, 위치
+	queue<pair<int, int> > q;   
+
+	//// 하나 넣고 시작
+	//q.push({ truck_weights[ind], ind });
+	//total_weight += truck_weights[ind];
+	//ind++;
+
+	while (1) {
+		if (ind > truck_weights.size()) break;
+		//시간 증가
+		++time;
+
+		//다리에 아무것도 없을때
+		if (q.empty()) {
+			q.push({ truck_weights[ind], ind });
+			total_weight += truck_weights[ind];
+			ind++;
+		}
+
+		// 다리에 있는것 한 칸 옮기기
+		else {
+			int size = q.size();
+			for (int i = 0; i < size; ++i) {
+				int wei = q.front().first;
+				int loc = q.front().second + 1;
+				q.pop();
+
+				if (loc <= bridge_length)
+					q.push({ wei, loc });
+			}
+
+			if (total_weight + truck_weights[ind] <= weight) {
+				q.push({ truck_weights[ind], ind });
+				total_weight += truck_weights[ind];
+				++ind;
+			}
+
+		}
+
+	}
+
+	cout << time;
+	return time;
+}
+int main() {
+	/*
+2	10	[7,4,5,6]	8
+100	100	[10]	101
+100	100	[10,10,10,10,10,10,10,10,10,10]	110
+	*/
+
+	return solution(2, 10, { 7, 4, 5, 6 });
+}
