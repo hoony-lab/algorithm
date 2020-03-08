993a2972ffdfcb57e57fd93666f95f32d5ef0713
hooooony <jihun9838@gmail.com>
2020-03-08 15:52:41 +0900

    boj 14500 테크로미노
    boj 14500 테크로미노

---
+++ Algorithm/boj 14500 테트로미노.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int dy[] = {};
+int dx[] = {  };
+
+int main() {
+	/*
+	ㅁ 회전 불필요
+	ㅡ, ㄹ 회전 두번
+	ㄴ, ㅗ 회전 네번
+
+	회전을 어떻게 표현하면 좋을까
+	- 하드코딩 ? 일단 생각난거로 해보기
+
+	회전시키고 맵에 들어갔는지
+
+
+	*/
+	return 0;
+}
