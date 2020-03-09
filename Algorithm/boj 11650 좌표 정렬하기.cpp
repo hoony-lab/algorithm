1aa740afade4dbe67c9952d06db3fbed096b213a
hooooony <jihun9838@gmail.com>
2020-03-09 18:23:09 +0900

    boj 11650 좌표 정렬하기
    boj 11650 좌표 정렬하기

---
+++ Algorithm/boj 11650 좌표 정렬하기.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define FOR(i,n) for(int i = 0 ; i < n ; ++i)
+#define endl '\n'
+int main() {
+
+	int n,x,y;
+	std::vector<std::pair<int, int>> v;
+	std::cin >> n;
+	
+	FOR(i, n) {
+		std::cin >> x >> y;
+		v.push_back(std::make_pair(x, y));
+	}
+
+	sort(v.begin(), v.end(), [](std::pair<int, int> v1, std::pair<int, int> v2) {
+		if (v1.first == v2.first) return v1.second < v2.second;
+		else return v1.first < v2.first;
+	});
+
+	FOR(i, n) {
+		std::cout << v[i].first << " " << v[i].second << endl;
+	}
+
+}
