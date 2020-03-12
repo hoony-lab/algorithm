435d3779780cbe848678f8eb32b92060218c9232
hooooony <jihun9838@gmail.com>
2020-03-12 15:09:29 +0900

    boj 11866 요세푸스 문제 0
    modify boj 11866 요세푸스 문제 0

--- Algorithm/boj 11866 요세푸스 문제 0.cpp
+++ Algorithm/boj 11866 요세푸스 문제 0.cpp
@@ -1,44 +1,42 @@
 #include <iostream>
 #include <string>
-using namespace std;
+#include <vector>
+#include <algorithm>
+
+void print(std::vector<int> v, int n) {
+	std::cout << "<";
+	for (int i = 0 ; i < n - 1; ++i )std::cout << v[i] << ", ";
+	std::cout << v[n - 1] << ">";
+}
 
 int main() {
-
-	std::string s, ans;
-	int n, k;
+	std::vector<int> v, ans;
+	std::vector<int>::iterator low, up;
+	int n, k, ind = 0;
 	std::cin >> n >> k;
 
-	for (int i = 1; i <= n; ++i) s.append(std::to_string(i));
-	
-	for (int i = 1; i <= n; ++i) {
-		//ans += s[3];
-		string aa = s.erase(s.find('3'),1);
-		std::cout << aa << '\n';
-		
+	for (int i = 1; i <= n; ++i) v.push_back(i);
+
+	while (v.size()) {
+		ind += k-1;
+		ind %= v.size();
+
+		ans.push_back(v[ind]);
+		v.erase(v.begin() + ind);
 	}
-	
-
-		//ans += s.erase(s.find[s[3]]);
-
-		std::cout << ans << ", ";
-
-	/*std::cout << "<";
-	for (int i = 0; i < n - 1; ++i)
-		std::cout << ans[i] << ", ";
-	std::cout << ans[n - 1] << ">";
-*/
+	print(ans, n);
 
 }
-	/*
-	123456712 / 345671234567
-	  3  6  2  5  1  4  7
+/*
+123456712 / 345671234567
+  3  6  2  5  1  4  7
 
-	1234567
-	12 4567	3
-	12 45 7	6
-	1  45 7	2
-	1  45	7
-	1  4	5
-	   4	1
-			4
-	*/
+1234567
+12 4567	3
+12 45 7	6
+1  45 7	2
+1  45	7
+1  4	5
+   4	1
+		4
+*/
