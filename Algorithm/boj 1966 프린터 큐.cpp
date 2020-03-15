73ebbcbbdbcf0112b41a6fd234762e9f69abb912
hooooony <jihun9838@gmail.com>
2020-03-15 14:54:52 +0900

    boj 1966 프린터 큐
    boj 1966 프린터 큐

--- Algorithm/boj 1966 프린터 큐.cpp
+++ Algorithm/boj 1966 프린터 큐.cpp
@@ -1,20 +1,55 @@
 #include <iostream>
 #include <queue>
 #include <deque>
+#include <vector> 
+#include <algorithm>
+
 int main() {
 	int t;
 	std::cin >> t;
 
 	while (t--) {
-		std::deque<int> deq;
-		int n, m;
+		std::queue<std::pair<int, int>> q;
+		std::priority_queue<int> pq;
+
+		int n, m, tmp, cnt = 0;
 		std::cin >> n >> m;
 
-		for (int i = 1; i <= n; ++i) {
-
+		for (int i = 0; i < n; ++i) {
+			std::cin >> tmp;
+			q.push(std::make_pair(tmp, i));
+			pq.push(tmp);
 		}
 
+		while (1) {
+			auto p = q.front();
+			q.pop();
+
+			if (p.first == pq.top()) {
+				pq.pop();
+				cnt++;
+				if (p.second == m) break;
+			}
+			else {
+				q.push(p);
+			}
+		}
+		std::cout << cnt << '\n';
 	}
 
-
 }
+/*
+3
+1 0
+5
+4 2
+1 2 3 4
+6 0
+1 1 9 1 1 1
+
+1 9 1 1 1 1
+
+9 1 1 1 1 1
+1 1 1 1 1
+
+*/
