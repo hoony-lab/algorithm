dda460ad9528e317a80f739e30868f557863a02a
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-03-18 10:03:16 +0900

    boj 2638 치즈
    boj 2638 치즈 right answer
    bfs

--- Algorithm/boj 2638 치즈.cpp
+++ Algorithm/boj 2638 치즈.cpp
@@ -6,79 +6,77 @@
 int dx[] = { 0,1,0,-1 };
 int dy[] = { 1,0,-1,0 };
 int n, m, time;
-int map[101][101], map_c[101][101];
-std::queue<std::pair<int, int>> q, qair;
+int map[101][101], visited[101][101];
+std::queue<std::pair<int, int>> qair;
+
+void print(int map[][101]) {
+	std::cout << '\n';
+	FOR(x, n, 0) {
+		FOR(y, m, 0) {
+			std::cout << map[x][y] << " ";
+		}std::cout << '\n';
+	}std::cout << '\n';
+}
+
 int main() {
 
-	time = 0;
 	std::cin >> n >> m;
-	FOR(x, n, 0)	FOR(y, m, 0) {
+	FOR(x, n, 0)	FOR(y, m, 0) 
 		std::cin >> map[x][y];
-		if (map[x][y]) q.push(std::make_pair(x, y));
-	}
-
 
 	while (++time) {
 
-		if (q.empty()) {
-			std::cout << time - 1;
+		//반대로 생각해서 외부 공기 접촉 치즈만 검사 후 제거
 
-			return 0;
-		}
-		int qsize = q.size();
+		memset(visited, 0, sizeof(visited));
 
-		memset(map_c, 0, sizeof(map_c));
-	
+		qair.push(std::make_pair(0, 0));
+		visited[0][0] = 1;
 
-		//외부 공기 비접촉 공기 검사
-		//가장자리에는 치즈가 놓이지 않는다는 가정
-		FOR(x, n, 2)	FOR(y, m, 2) {
-			if (map[x][y]) continue;
-			qair.push(std::make_pair(x, y));
-			map[x][y] = -1;
-
-			while(!qair.empty()) {
-				int tx = q.front().first;
-				int ty = q.front().second;
-				qair.pop();
+		while (!qair.empty()) {
+			int tx = qair.front().first;
+			int ty = qair.front().second;
+			qair.pop();
 			
-				FOR(a, 4, 0) {
-					int nx = tx + dx[a];
-					int ny = ty + dy[a];
-
-					if (nx < 0 || ny < 0 || nx >= n || ny >= m || map[nx][ny] != 0) continue;
-
-					qair.push(std::make_pair(nx, ny));
-					map[nx][ny] = -1;
-				}
-			}
-		}
-
-
-		//녹는 치즈 검사
-		FOR(i, qsize, 0) {
-			int tx = q.front().first;
-			int ty = q.front().second;
-			q.pop();
-
-			int count = 0;
 			FOR(a, 4, 0) {
 				int nx = tx + dx[a];
 				int ny = ty + dy[a];
+				if (nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny]) continue;
 
-				if (nx < 0 || ny < 0 || nx >= n || ny >= m || map[nx][ny] != 0) continue;
-				count++;
+				if (map[nx][ny] == 0) {
+					qair.push(std::make_pair(nx, ny));
+					visited[nx][ny] = 1;
+				}
+				else if (map[nx][ny] >= 1) map[nx][ny]++;
 			}
-
-			if (count >= 2) map_c[tx][ty] += 1;
-			else q.push(std::make_pair(tx, ty));
 		}
 
-		//치즈 녹이기
+		//print(map);
+
+		bool done = true;
 		FOR(x, n, 0)	FOR(y, m, 0) {
-			if (map_c[x][y]) map[x][y] -= map_c[x][y];
+			if (map[x][y] >= 3)	map[x][y] = 0, done = false;
+			else if (map[x][y] == 2) map[x][y] = 1, done = false;
+		}
+
+		if (done) {
+			std::cout << time - 1; 
+			
+			return 0;
 		}
 	}
-
-
 }
+
+/*
+
+8 9
+0 0 0 0 0 0 0 0 0
+0 0 0 1 1 0 0 0 0
+0 0 0 1 1 0 1 1 0
+0 0 1 1 1 1 1 1 0
+0 0 1 1 1 1 1 0 0
+0 0 1 1 0 1 1 0 0
+0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
+
+*/
