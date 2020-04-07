d0dd599c8f16e6a0e5187e45149de93e1687b78e
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-04-07 16:52:57 +0900

    Update boj 17836 공주님을 구해라!.cpp
    21 times of error drove me crazy

--- Algorithm/boj 17836 공주님을 구해라!.cpp
+++ Algorithm/boj 17836 공주님을 구해라!.cpp
@@ -5,27 +5,32 @@
 
 int dy[] = { 1,0,-1,0 };
 int dx[] = { 0,1,0,-1 };
-int n, m, t, time = 0;
+int n, m, t, time = 0, find_sword_time, find_princess_without_sword, swordx, swordy, find_princess, ans = 1e9;
 int map[101][101], visit[101][101];
 std::queue<std::pair< int, int>> q;
 
 void print(int map[][101], int n, int m) {
 	FOR(x, n) { FOR(y, m) { std::cout << map[x][y] << " "; } std::cout << '\n'; } std::cout << '\n';
 }
+
 int main() {
+	memset(map, 0, sizeof(map));
+	memset(visit, 0, sizeof(visit));
+
 	std::cin >> n >> m >> t;
-	FOR(x, n) FOR(y, m) std::cin >> map[x][y];
+	 
+	FOR(x, n) FOR(y, m) {
+		std::cin >> map[x][y];
+		if (map[x][y] == 2) swordx = x, swordy = y;
+	}
 
 	q.push({ 0, 0 });
-	visit[0][0] = true;
+	visit[0][0] = 0;
 
 	while (!q.empty()) {
 		time++;
-		if (time > t) {
-			std::cout << "Fail";
-			return 0;
-		}
-		//std::cout << " @ time : " << time << '\n';
+		
+		//std::cout << " @ time : " << time << '\n';		
 
 		int qsize = q.size();
 		FOR(i, qsize) {
@@ -38,38 +43,72 @@ int main() {
 				int ny = ty + dy[a];
 
 				if (nx < 0 || ny < 0 || nx >= n || ny >= m || visit[nx][ny] || map[nx][ny] == 1) continue;
-				//std::cout << nx << " " << ny << " " << tz << '\n';
 
-				//// saved the princess, answer
+				q.push({ nx, ny });
+				visit[nx][ny] = visit[tx][ty] + 1;
+
+				//// saved the princess
 				if (nx == n - 1 && ny == m - 1) {
-					std::cout << time;
-					return 0;
+					find_princess_without_sword = visit[nx][ny];
+					find_princess = true;
+					ans = ans < find_princess_without_sword ? ans : find_princess_without_sword;
+					break;
 				}
 
-				if (map[nx][ny] == 0) {
-					q.push({ nx, ny });
-					visit[nx][ny] = visit[tx][ty] + 1;
-				}
 				//// find gram sword
-				else if (map[nx][ny] == 2) {
-					//// cannot reach the princess, Fail
-					if (time + (n - 1 - nx) + (m - 1 - ny) > t) std::cout << "Fail";
-					else std::cout << time + (n - 1 - nx) + (m - 1 - ny);
-					return 0;
+				if (map[nx][ny] == 2 && !find_sword_time) {
+					find_sword_time = time;
+					if (find_sword_time + (n - 1 - swordx) + (m - 1 - swordy) > t) continue;
+					ans = ans < find_sword_time + (n - 1 - swordx) + (m - 1 - swordy) ? ans : find_sword_time + (n - 1 - swordx) + (m - 1 - swordy);
 				}
+				
 			}
-			//// timeover, Fail
-			
-			
+			if (find_princess) break;
 		}
-		//std::cout << "print \n";
-		//print(visit, n, m, 0);
-		//std::cout << "\n";
-		//print(visit, n, m, 1);
+		if (find_princess) break;
+
+		//print(visit, n, m);
 	}
-	//// cannot reach the princess, Fail
-	if (!visit[n - 1][m - 1] || !visit[n - 1][m - 1] || time > t)
-		std::cout << "Fail";
+
+	if (ans > t)	std::cout << "Fail";
+	else			std::cout << ans;
 
 	return 0;
 }
+/*
+
+5 5 14
+0 1 0 0 0
+0 1 2 1 0
+0 1 0 0 0
+0 1 0 1 0
+0 0 0 1 0
+
+6 6 16
+0 0 0 0 1 1
+0 0 0 0 0 2
+1 1 1 0 1 0
+0 0 0 0 0 0
+0 1 1 1 1 1
+0 0 0 0 0 0
+
+
+7 7 16
+0 0 0 0 1 1 0
+0 0 0 0 0 2 0
+1 1 1 0 1 0 0
+0 0 0 0 0 0 0
+0 1 1 1 1 1 1
+0 0 0 0 0 0 0
+0 0 0 0 0 0 0
+
+
+7 7 24
+0 0 0 0 1 1 0
+0 0 0 0 0 0 0
+1 1 1 1 1 1 0
+0 0 0 0 2 0 0
+0 1 1 1 1 1 1
+0 0 0 0 0 1 0
+0 0 0 0 0 1 0
+*/
