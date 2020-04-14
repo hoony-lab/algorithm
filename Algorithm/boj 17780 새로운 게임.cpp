04e6b20d22b3f88d9db2043515a3f80a05705110
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-04-14 14:18:43 +0900

    Update boj 17780 새로운 게임.cpp
    int map [][]
    deque<int> [][]
    vector<struct { x, y, way } >

--- Algorithm/boj 17780 새로운 게임.cpp
+++ Algorithm/boj 17780 새로운 게임.cpp
@@ -8,53 +8,105 @@
 
 // R L U D
 int dx[] = { 0, 0, -1, 1 };
-int dy[] = { 1, -1, 0, 0};
+int dy[] = { 1, -1, 0, 0 };
 
-struct pos {
-	std::vector<std::pair<int, int>> v;
+struct Chess {
+	int x, y, way;
+};
+
+int n, k, temp, ans, map[13][13], upside_down[13], limit = 1000;
+std::deque<int> dq[13][13];
+std::vector<Chess> v;
+
+void print() {
+	FOR(x, n) {
+		FOR(y, n) {
+			if (dq[x][y].empty())			std::cout << "x";
+			else for (auto i : dq[x][y])	std::cout << i;
+			std::cout << "\t";
+		}std::cout << '\n';
+	}std::cout << '\n';
 }
 
 int main() {
-	int n, k, map[13][13];
 	std::cin >> n >> k;
-	
+
 	FOR(x, n) FOR(y, n) std::cin >> map[x][y];
 
 	FOR(i, k) {
 		int xx, yy, way;
 		std::cin >> xx >> yy >> way;
-	
+
+		v.push_back({ xx - 1, yy - 1 , way - 1 });
+		dq[xx - 1][yy - 1].push_back(i);
 	}
 
+	while (limit--) {
+		ans++;
 
+		FOR(i, k) {
+			int tx = v[i].x;
+			int ty = v[i].y;
+			int tway = v[i].way;
 
+			// 맨 아래층 확인
+			if (dq[tx][ty].front() != i) continue;
 
-	std::deque<std::pair<int, int>> q;
-	q.push_front(std::make_pair(0, 0));
-	
-	while (!q.empty()) {
-		int tx = q.front().first;
-		int ty = q.front().second;
-		q.pop_front();
+			int nx = tx + dx[tway];
+			int ny = ty + dy[tway];
 
-		FOR(a, 4) {
-			int nx = tx + dx[a];
-			int ny = ty + dy[a];
+			//std::cout <<ans << " V i : " << i << " << " <<  tx << ", " << ty << " : " << nx << ", " << ny << " :: " << tway <<'\n';
 
-			if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+			// 파랑색 혹은 벽
+			if (map[nx][ny] == 2 || nx < 0 || ny < 0 || nx >= n || ny >= n) {
+				if (tway == 0 || tway == 2)		tway += 1;
+				else if(tway == 1 || tway == 3)	tway -= 1;
 
-			q.push_front({ nx,ny });
-			
-			if (map[nx][ny] == 0) {
+				nx = tx + dx[tway];
+				ny = ty + dy[tway];
+				
+				v[i].way = tway;
 
+				// 또 파랑색 혹은 벽
+				if (map[nx][ny] == 2 || nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
 			}
-			else if (map[nx][ny] == 1) {
 
-			}
-			else if (map[nx][ny] == 2) {
+			// 이동
+			if (map[nx][ny] == 1) {
+				while (!dq[tx][ty].empty()) {
 
+					temp = dq[tx][ty].back();
+					dq[tx][ty].pop_back();
+
+					dq[nx][ny].push_back(temp);
+					v[temp].x = nx;
+					v[temp].y = ny;
+				}
 			}
+			else if(map[nx][ny] == 0 /*|| map[nx][ny] == 2*/) {
+				while (!dq[tx][ty].empty()) {
+
+					temp = dq[tx][ty].front();
+					dq[tx][ty].pop_front();
+
+					dq[nx][ny].push_back(temp);
+					v[temp].x = nx;
+					v[temp].y = ny;
+				}
+			}
+
+			// 4개 확인
+			int dqsize = dq[nx][ny].size();
+			if (dqsize >= 4) {
+				std::cout << ans;
+
+				return 0;
+			}
+			//print();
 		}
+
 	}
+	if (ans >= 1000) std::cout << -1;
+
 	return 0;
-}{
+}
