620eb5e4b5f40055771e32b6cc135d4c19a85f43
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-04-16 17:59:41 +0900

    Create boj 1049 마피아.cpp
    dfs, brute-force
    
    TLE

---
+++ Algorithm/boj 1049 마피아.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#define FIO std::ios_base::sync_with_stdio(false), std::cin.tie(NULL)
+#define FOR(i,a,b) for(int i = a; i < b; ++i)
+
+int n, guilty[30], g_index[30][30], eunjin;
+int is_dead[30], ans;
+
+
+int most_guilty(int guilty[]) {
+	int max = 0, res = 0;
+	FOR(i, 0, n) {
+		if (is_dead[i] || eunjin == i) continue;
+		if (max < guilty[i]) max = guilty[i], res = i;
+	}
+	return res;
+}
+
+void dfs(int guilty[], int g_index[][30], int is_dead[], int turn, int ppl) {
+	int guilty_c[30], g_index_c[30][30];
+
+	if (ppl == 1) {
+		if(ans < turn) ans = turn;
+		return;
+	}
+
+	// day
+	// kill most guilty person
+	if (ppl & 1) {
+		int morning_dead = most_guilty(guilty);
+
+		is_dead[morning_dead] = true;
+		dfs(guilty_c, g_index_c, is_dead, turn, ppl - 1);
+		is_dead[morning_dead] = false;
+	}
+
+	// night
+	// kill random person (brute-force)
+	else {
+		FOR(i, 0, n) {
+			if (is_dead[i] || eunjin == i) continue;
+
+			memcpy(guilty_c, guilty, sizeof(guilty));
+			memcpy(g_index_c, g_index, sizeof(g_index));
+			
+			FOR(j, 0, n) 
+				guilty_c[j] += g_index_c[i][j];
+			
+			is_dead[i] = true;
+			dfs(guilty_c, g_index_c, is_dead, turn + 1, ppl - 1);
+			is_dead[i] = false;
+		}
+	}
+}
+
+int main() {
+	FIO;
+
+	std::cin >> n;
+	FOR(i, 0, n) std::cin >> guilty[i];
+	FOR(x, 0, n) FOR(y, 0, n) std::cin >> g_index[x][y];
+	std::cin >> eunjin;
+	
+	dfs(guilty, g_index, is_dead, 0, n);
+
+	std::cout << ans;
+
+	return 0;
+}
+/*
+4
+500 500 500 500
+1 4 3 -2
+-2 1 4 3
+3 -2 1 4
+4 3 -2 1
+1
+
+
+
+
+
+2
+¸Þ-
+*/
