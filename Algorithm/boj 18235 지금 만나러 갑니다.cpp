4d653ee21ebbab57ee674c55d6ad04a7971c2220
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-04-21 12:43:59 +0900

    Update boj 18235 지금 만나러 갑니다.cpp
    memory overflow
    - bfs with two positions
    
    -> bfs FiveDuck and SixDuck respectively

--- Algorithm/boj 18235 지금 만나러 갑니다.cpp
+++ Algorithm/boj 18235 지금 만나러 갑니다.cpp
@@ -1,49 +1,110 @@
 #include <iostream>
 #include <queue>
-int n, a, b, ans, jump, done;
+#include <cstring>
+int n, a, b, ans = 1e9;
+bool visitf[500001][30], visits[500001][30], flag = false;
+int d[] = { -1, 1 };
 
 struct pos {
-	int five, six;
+	int pos, time;
 };
 
-int main() {
-	std::cin >> n >> a >> b;
-
-	std::queue<pos> q;
-	q.push({ a, b });
+void bfs(std::queue<pos> q) {
 
 	while (!q.empty()) {
 
 		int qsize = q.size();
-		for (int i = 0; i < qsize || !done; ++i) {
+		for (int i = 0; i < qsize; ++i) {
+
+			int pos = q.front().pos;
+			int time = q.front().time;
+			q.pop();
+
+			//std::cout << "pos : " << pos << " : " << time << '\n';
+
+			for (int a = 0; a < 2; ++a) {
+				int npos = pos + (1 << time) * d[a];
+				int ntime = time + 1;
+				if (npos < 1 || npos > n) continue;
+
+				if (!flag) {
+					if (visitf[npos][ntime]) continue;
+					else visitf[npos][ntime] = true;
+				}
+				else {
+					if (visits[npos][ntime]) continue;
+					else {
+						if (visitf[npos][ntime]) {
+							ans = ntime;
+							return ;
+						}
+						else visits[npos][ntime] = true;
+					}
+				}
+
+				q.push({ npos, time + 1 });
+			}
+		}
+	}
+}
+
+
+int main() {
+	std::cin >> n >> a >> b;
+
+	memset(visits, false, sizeof(visits));
+	memset(visitf, false, sizeof(visitf));
+
+	std::queue<pos> qf, qs;
+	qf.push({ a, 0 }), qs.push({ b, 0 });
+	visitf[a][0] = visits[b][0] = true;
+
+	bfs(qf);
+	flag = true;
+	bfs(qs);
+
+	if (ans == 1e9) std::cout << -1;
+	else std::cout << ans;
+
+	/*
+	while (!q.empty()) {
+		std::cout << "time : " << ans << " jump : " << jump << "\n\n";
+
+		int qsize = q.size();
+		for (int i = 0; i < qsize; ++i) {
 
 			int f = q.front().five;
 			int s = q.front().six;
 			q.pop();
+			std::cout << f << " : " << s << '\n';
 
-			if (f == s || jump > n) { done = true; break; }
+			for (int a = 0; a < 2; ++a) {
+				for (int b = 0; b < 2; ++b) {
+					int nf = f + jump * d[a];
+					int ns = s + jump * d[b];
 
-			if (f - jump > 0 && s - jump > 0)
-				q.push({ f - jump, s - jump });
+					if (nf < 1 || nf > n || ns < 1 || ns > n) continue;
+					if (visit[0][nf] || visit[1][ns]) continue;
 
-			if (f - jump > 0 && s + jump <= n)
-				q.push({ f - jump, s + jump });
+					if (nf == ns) {
+						std::cout << ans + 1;
+						return 0;
+					}
 
-			if (f + jump <= n && s - jump > 0)
-				q.push({ f + jump, s - jump });
-
-			if (f + jump <= n && s + jump <= n)
-				q.push({ f + jump, s + jump });
-
-			std::cout << ans;
+					q.push({ nf, ns });
+					visit[0][nf] = visit[1][ns] = ans;
+				}
+			}
 		}
 
-		jump << 1;
+		jump <<= 1;
+		if (jump > n) {
+			std::cout << -1;
+			return 0;
+		}
 		ans++;
-	}
+	}*/
 
-	if (jump > n) ans = -1;
-	std::cout << ans;
 
 	return 0;
 }
