fd1065685e0ef307f5d33772d2c8b261d8bc28fe
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-04-22 09:56:37 +0900

    Update boj 15961 회전 초밥.cpp
    sliding window (deque)
    - idk y it keeps going wrong
    -> just plus & minus with eat[]

--- Algorithm/boj 15961 회전 초밥.cpp
+++ Algorithm/boj 15961 회전 초밥.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <set>
 #include <deque>
-#include <cstring>
+
 #define MAX(a, b) a > b ? a : b
 #define FIO std::ios_base::sync_with_stdio(false), std::cin.tie(NULL);
 #define FOR(i, n, m) for(int i = m ; i < n ; ++i)
-int n, d, k, coupon, sushi_belt[3000001], ans = -1, four[3001], sum;
-std::set<int> sushi;
+
+int n, d, k, coupon, sushi_belt[3000001], ans, eat[3001], sum;
 std::deque<int> dq;
 /*
 연속된 초밥이면 할인
@@ -14,65 +13,49 @@ std::deque<int> dq;
 - 행사 한번 참여하면 초밥 하나 무료
 - 만약 행사 초밥이 없으면, 새로 만들어서 제공
 */
-void print() {
-	for (auto a : sushi)
-		std::cout << a << ", ";
-	std::cout << '\n';
-}
 
 int main() {
 	FIO;
+
 	std::cin >> n >> d >> k >> coupon;
 	FOR(i, n, 0) std::cin >> sushi_belt[i];
 
-	memset(sushi_belt, 0, sizeof(sushi_belt));
-	memset(four, 0, sizeof(four));
-
-	//// sushi start num
-	//FOR(num, n) {
-
-	//	sushi.clear();
-
-	//	FOR(i, k) {
-	//		int s = (num + i) % d;
-	//		sushi.emplace(sushi_belt[s]);
-	//	} 
-
-	//	//std::cout << "ENTER at : " << num << '\n';
-	//	//print();
-
-	//	if (ans <= sushi.size()) {
-	//		ans = sushi.size();
-	//		if (!sushi.count(coupon)) ans++;
-	//		if (ans == k + 1) break;
-	//	}
+	//FOR(i, n, n - k) {
+	//	dq.push_front(sushi_belt[i]);
+	//	if (!eat[sushi_belt[i]]) sum++;
+	//	eat[sushi_belt[i]]++;
 	//}
-	//std::cout << ans;
+
+	//FOR(i, n, 0) {
+	//	int dqfront = dq.front();
+	//	eat[dqfront]--;
+	//	dq.pop_front();
+	//	if (!eat[dqfront]) sum--;
+
+	//	dq.push_back(sushi_belt[i]);
+	//	if (!eat[sushi_belt[i]]) sum++;
+	//	eat[sushi_belt[i]]++;
 
 
-	FOR(i, n, n - k) {
-		dq.push_front(sushi_belt[i]);
-		if (!four[sushi_belt[i]]) sum++;
-		four[sushi_belt[i]]++;
-		
-	}
+	//	if (!eat[coupon])	ans = MAX(ans, sum + 1);
+	//	else				ans = MAX(ans, sum);
+	//}
+
+	FOR(i, k, 0) 
+		if (!eat[sushi_belt[i]]++)  sum++;
+	
 	FOR(i, n, 0) {
-		four[dq.front()]--;
-		if (!four[dq.front()]) sum--;
-		dq.pop_front();
+		if (!--eat[sushi_belt[i]]) sum--;
+		if (!eat[sushi_belt[(i + k) % n]]++)  sum++;
 
-		dq.push_back(sushi_belt[i]);
-		if (four[sushi_belt[i]]) sum++;
-		four[sushi_belt[i]]++;
-
-
-		if (!four[coupon])	ans = MAX(ans, sum + 1);
+		if (!eat[coupon])	ans = MAX(ans, sum + 1);
 		else				ans = MAX(ans, sum);
 	}
 
 	std::cout << ans;
 	return 0;
 }
+
 /*
 8 50 4 7
 2
