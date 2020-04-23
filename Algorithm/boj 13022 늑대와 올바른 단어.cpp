d1ac39182037bc08b32cd4bd9b51f5f00c8b59e5
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-04-23 18:18:05 +0900

    Update boj 13022 늑대와 올바른 단어.cpp
    string, find, idx pointer
    - ????
    - !!!!

--- Algorithm/boj 13022 늑대와 올바른 단어.cpp
+++ Algorithm/boj 13022 늑대와 올바른 단어.cpp
@@ -1,43 +1,55 @@
 #include <iostream>
 #include <string>
-#define W 0
-#define O 1
-#define L 2
-#define F 3
-int ans, wolf[4], flag;
+#define WRONG return !std::cout << 0;
 
 int main() {
+	int start = 0;
 	std::string s; std::cin >> s;
 
-	for (int i = 0; i < s.size(); ++i) {
-
-		if (i && s[i - 1] == 'f' && s[i] != 'w') {
-			if (flag) {
-				flag = false;
-				for (int a = 1; a < 4; ++a)
-					if (wolf[a - 1] != wolf[a]) return !std::cout << 0;
-
-			}
-		}
-		else if (s[i] == 'w') wolf[W]++;
-		else if (s[i] == 'o') wolf[O]++;
-		else if (s[i] == 'l') wolf[L]++;
-		else if (s[i] == 'f') wolf[F]++, flag = true;
-
-
-		//else if (s[i] == 'o') 
-		//	if (wolf[W] > wolf[O]) wolf[O]++; 
-		//	else return !std::cout << 0;
-
-		//else if (s[i] == 'l')
-		//	if (wolf[O] > wolf[W]) wolf[L]++;
-		//	else return !std::cout << 0;
-
-		//else if (s[i] == 'f')
-		//	if (wolf[L] > wolf[F]) wolf[F]++; 
-		//	else return !std::cout << 0;
+	if (s[0] != 'w' || s[s.length() - 1] != 'f' || s.length() % 4 != 0) {
+		std::cout << 0; return 0;
 	}
 
-	std::cout << 1;
-	return 0;
+	while (start < s.length()) {
+
+		int w = s.find('w', start);
+		int o = s.find('o', start);
+		int l = s.find('l', start);
+		int f = s.find('f', start);
+
+		int wo = o - w;
+		int ol = l - o;
+		int lf = f - l;
+		int fw;
+
+		int nextw = s.find('w', f);
+
+		// 마지막 단어
+		if (nextw == -1) {
+			start = f + wo;
+			fw = wo;
+		}
+		else {
+			start = nextw;
+			fw = nextw - f;
+		}
+
+		if (w >= o || o >= l || l >= f ||
+			wo != ol || ol != lf || wo != lf || wo != fw ||
+			w < 0 || o < wo || l < wo + ol || f < wo + ol + lf || start < wo + ol + lf + fw) {
+			std::cout << 0; return 0;
+		}
+	}
+
+	std::cout << 1; return 0;
 }
+
+/*
+wolf
+1
+wwolfolf
+0
+
+//23%
+
+*/
