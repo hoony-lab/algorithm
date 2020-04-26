832b6ad962c9833f2d8cf8756dbb154a92d597e6
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-04-26 13:19:16 +0900

    Update 2019 kakao blind recruitment 블록 게임.cpp
    에휴

--- Algorithm/2019 kakao blind recruitment 블록 게임.cpp
+++ Algorithm/2019 kakao blind recruitment 블록 게임.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
-
+#include <set>
 #define F(i, n) for(int i = 0 ; i < n ; ++i)
 #define FR(i, n) for(int i = n - 1 ; i >= 0; --i)
 #define BLOCKMAX 201
@@ -10,6 +10,7 @@ using namespace std;
 
 // ¤¤ J L ¤Ò ¤Ç
 // start from the bottom which has no upper block
+// right to left
 //int blockx[5][3] = {
 //	{0, 0, -1},
 //	{0,-1, -2},
@@ -38,6 +39,8 @@ int blocky[5][4] = {
 
 int n, opened_block[BLOCKMAX], answer;
 vector<vector<int>> map;
+vector<pair<int, int>> block[BLOCKMAX];
+set<int> block_num;
 
 void print() {
 	F(x, n) {
@@ -68,16 +71,37 @@ bool is_opened(int x, int y) {
 	return true;
 }
 
-bool check_block_shape(int x, int y) {
+bool is_opened2(int num) {
+	int x = block[num].front().first;
+	int y = block[num].front().second;
 
+	for (auto a : block[num]) {
+		int nx = a.first, ny = a.second;
+
+		// move up
+		while (check(--nx, ny)) {
+
+			// if map is 0 or same as starting point, move up
+			if (map[nx][ny] == 0 || map[nx][ny] == map[x][y]) continue;
+
+			opened_block[map[x][y]] = false;
+			return false;
+		}
+		return true;
+	}
+}
+
+bool check_block_shape(int x, int y) {
+	//cout << "check shape : " << x << " ," << y << '\n';
 	// check 5 possible shape
 	F(a, 5) {
 		bool fail = false;
+
 		F(b, 4) {
 
 			// if map is not same as starting point or closed
 			int nx = x + blockx[a][b], ny = y + blocky[a][b];
-			if (!check(nx, ny) || map[nx][ny] != map[x][y] || !is_opened(nx,ny))
+			if (!check(nx, ny) || map[nx][ny] != map[x][y] || !is_opened(nx, ny))
 				fail = true;
 		}
 
@@ -87,8 +111,8 @@ bool check_block_shape(int x, int y) {
 			F(b, 4) map[x + blockx[a][b]][y + blocky[a][b]] = 0;
 
 			// debugging
-			cout << "Done at " << x << ", " << y << " : " << "shape : " << a <<"\n";
-			print();
+			//cout << "Done at " << x << ", " << y << " : " << "shape : " << a << "\n";
+			//print();
 
 			return true;
 		}
@@ -96,23 +120,87 @@ bool check_block_shape(int x, int y) {
 	return false;
 }
 
+bool check_block_shape2(int num) {
+	int x = block[num].front().first;
+	int y = block[num].front().second;
+
+	F(a, 5) {
+		bool fail = false;
+
+		F(b, 4) {
+			int nx = x + blockx[a][b];
+			int ny = y + blocky[a][b];
+
+			if (nx != block[num][b].first || ny != block[num][b].second || !is_opened(nx, ny))
+				fail = true;
+		}
+
+		if (!fail) {
+
+			// erase number on the erasable block
+			F(b, 4) map[x + blockx[a][b]][y + blocky[a][b]] = 0;
+			block_num.erase(num);
+
+			// debugging
+			//cout << "Done at " << x << ", " << y << " : " << "shape : " << a << "\n";
+			//print();
+
+			return true;
+		}
+	}
+	return false;
+}
+
+bool possible_shape(int x, int y) {
+	F(a, 5) {
+		bool fail = false;
+		F(b, 4) {
+			int nx = x + blockx[a][b];
+			int ny = y + blocky[a][b];
+
+			if (!check(nx, ny) || map[x][y] != map[nx][ny])
+				fail = true;
+		}
+		if (!fail) return true;
+	}
+	return false;
+}
 
 void solve() {
 	memset(opened_block, true, sizeof(opened_block));
 
-	FR(x, n) FR(y, n) {
-		if (!map[x][y]) continue;
-		if (!check_block_shape(x, y)) continue;
+	//FR(x, n) FR(y, n) {
+	for (int num : block_num) {
+		//if (!check_block_shape(x, y)) continue;
+		//if (!check_block_shape2(num)) continue;
+
+		if (!is_opened2(num)) continue;
 
 		answer++;
 		solve();
+
+		return;
 	}
+
 }
 
 int solution(vector<vector<int>> board) {
 	map.clear();		map = board;
 	n = board.size();
 
+	FR(x, n) FR(y, n) {
+		if (!map[x][y]) continue;
+		if (possible_shape(x, y))
+			block_num.insert(map[x][y]);
+
+		block[map[x][y]].push_back(make_pair(x, y));
+
+	}
+	std::cout << "block_num.size() " << block_num.size() << '\n';
+	for (int num : block_num) {
+		cout << block[num].size() << " < ";
+		cout << block[num].front().first << ", " << block[num].front().second << '\n';
+	}
 	solve();
 
 	return answer;
@@ -126,8 +214,8 @@ int solution(vector<vector<int>> board) {
 int main() {
 
 	vector<vector<int>> board = {
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 6, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 11, 0, 0, 0},
+		{0, 0, 0, 0, 6, 11, 11, 11, 0, 0},
 		{0, 0, 10, 6, 6, 6, 0, 0, 0, 0},
 		{0, 10, 10, 10, 0, 0, 0, 0, 0, 0},
 		{7, 0, 0, 9, 0, 0, 4, 0, 0, 8},
@@ -138,6 +226,6 @@ int main() {
 		{1, 1, 1, 0, 0, 0, 0, 0, 0, 5}
 	};
 	cout << solution(board);	// 2
-	
+
 	return 0;
-} 
+}
