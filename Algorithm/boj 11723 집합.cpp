5958f8b2ffaec414bd984d5479fe785d8c696afd
hooooony <jihun9838@gmail.com>
2020-05-10 17:17:23 +0900

    Update boj 11723 집합.cpp
    string
    set
    - ez

--- Algorithm/boj 11723 집합.cpp
+++ Algorithm/boj 11723 집합.cpp
@@ -1,48 +1,50 @@
 #include <iostream>
 #include <set>
 #include <string>
+#include <sstream>
+//#include <istream>
+//#include <ostream>
+
 int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
 	std::set<int> sett;
-	std::string s1, s2;
+	std::string s;
 	int t, n;
-	std::cin >> t; getchar();
+
+	std::cin >> t;
+
 	while (t--) {
 
-		std::getline(std::cin, s1, ' ');
-		if (s1 == "all") {
+		std::cin >> s;
+
+		if (s == "all") {
 			for (int i = 1; i < 21; ++i)
 				sett.insert(i);
 			continue;
 		}
-		else if (s1 == "empty") {
+		else if (s == "empty") {
 			sett.clear();
 			continue;
 		}
 
-		std::getline(std::cin, s2, '\n');
-		n = std::stoi(s2);
-		
-		std::cout << s1.length() << ", " << s2.length() << '\n';
+		std::cin >> n;
 
+		if (s == "add") sett.insert(n);
 
-		if (s1 == "add") sett.insert(n);
-
-		else if (s1 == "check") {
+		else if (s == "check") {
 			if (sett.count(n))	std::cout << 1 << '\n';
 			else				std::cout << 0 << '\n';
 		}
 
-		else if (s1 == "remove") sett.erase(n);
+		else if (s == "remove") sett.erase(n);
 
-		else if (s1 == "toggle") {
+		else if (s == "toggle") {
 			if (sett.count(n))	sett.erase(n);
 			else				sett.insert(n);
 		}
-
-
-
-		
 	}
-
 	return 0;
 }
