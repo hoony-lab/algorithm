e54c1aa9f00f02ca7764ec9511c4d3f555a1df83
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-05-16 23:25:39 +0900

    Update boj 3678 카탄의 개척자.cpp
    // 층, 변, 위치
    int blocks[MAX][6][MAX];
    
    	while (floor < MAX) {
    		F(side, 6) {
    			F(place, floor) {
    				//std::cout << "\n\nNUM : " << num++ << "   BLOCK : " << floor << ", " << side << ", " << place << '\n';
    				vi v = check_adjust_block(floor, side, place);
    				int new_block = min_count_block(v);
    
    				blocks[floor][side][place] = new_block;
    				count[new_block]++;
    				//std::cout << "ADD : " << new_block << '\n';
    			}
    		}
    		floor++;
    	}

--- Algorithm/boj 3678 카탄의 개척자.cpp
+++ Algorithm/boj 3678 카탄의 개척자.cpp
@@ -1,51 +1,68 @@
 #include <iostream>
 #include <queue>
 #include <algorithm>
-#include <map>
+
 #define F(i, n) for(int i = 0 ; i < n ; ++i)
 #define FE(i, n, m) for(int i = m ; i < n ; ++i)
 #define FR(i, n, m) for(int i = m ; i >= n ; --i)
 #define FIO std::ios_base::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
-struct P { int f, s, p; };
+#define MAX 81
+
 using vi = std::vector<int>;
-using vP = std::vector<P>;
+
 // 층, 변, 위치
-int blocks[80][6][80];
+int blocks[MAX][6][MAX];
 int count[6], block[10001];
 
-//((floor - 1) * 3 * floor) + (floor * side) + (place + 2)
+void out(std::string s, int a, int b, int c) {
+	std::cout << s << a << ", " << b << ", " << c << '\n';
+}
+void out(int n, int a, int b, int c) {
+	std::cout << n << " :: " << a << ", " << b << ", " << c << '\n';
+}
+
 int play(int num) {
 	if (num <= 7) {
 		if (num <= 5) return num;
 		if (num == 6) return 2;
 		return 3;
 	}
-	int start = 0, end = 80, floor, side, place;
+
+	int start = 0, end = MAX, floor, side, place;
 	while (start <= end) {
 		floor = (start + end) / 2;
 		int s = (floor - 1) * 3 * floor + 2, e = floor * 3 * (floor + 1) + 2;
 
-		std::cout << "Searching " << s << " < " << e << '\n';
+		//std::cout << s << " < " << e << '\n';
 
 		if (s <= num && num < e) break;
 		else if (num < s) end = floor - 1;
 		else if (e <= num) start = floor + 1;
 	}
 
-	num -= ((floor - 1) * 3 * floor) - 2;
+	num -= ((floor - 1) * 3 * floor) + 2;
 	side = num / floor;
 	place = num % floor;
-	std::cout << num << " :::::::::: " << "ANS : " << floor << ", " << side << ", " << place << "\n";
+
+	//out("Answer : ", floor, side, place);
+
 	return blocks[floor][side][place];
 }
 
 int min_count_block(vi v) {
-	int min_count = 1e9, min_index = 100;
-	FR(i, 1, 5)
-		if (count[i] <= min_count)
-			for (int num : v) if (num != i)
-				min_index = i;
+	//FE(a, 6, 1) std::cout << count[a] << ", ";
+	//std::cout << "\n";
 
+	int min_count = 1e9, min_index;
+	FR(i, 1, 5) if (count[i] <= min_count) {
+		bool nope = false;
+		for (int num : v) {
+			if (num == i) nope = true;
+		}
+		if (nope) continue;
+		min_count = count[i];
+		min_index = i;
+	}
 	return min_index;
 }
 /*
@@ -66,51 +83,93 @@ int min_count_block(vi v) {
 
 vi check_adjust_block(int floor, int side, int place) {
 	vi v;
-	if (side == 0 && place == 0) {		// 새로운 시작
-		v.push_back(blocks[floor - 1][5][floor - 2]);
+
+	//out("Check : ", floor, side, place);
+
+	if (side == 0 && place == 0) {		/// 새로운 시작
+
 		v.push_back(blocks[floor - 1][0][0]);
+		v.push_back(blocks[floor - 1][5][floor - 2]);
+
+		//out(blocks[floor - 1][0][0], floor - 1, 0, 0);
+		//out(blocks[floor - 1][5][floor - 2], floor -1, 5, floor - 2);
 	}
-	else if (place == floor - 1) {		// 새로운 변의 꼭짓점 or 변의 마지막
+	else if (place == floor - 1) {		/// 새로운 변의 꼭짓점 or 변의 마지막
 		v.push_back(blocks[floor][side][place - 1]);
 		v.push_back(blocks[floor - 1][side][floor - 2]);
+
+		//out(blocks[floor][side][place - 1], floor, side, place -1);
+		//out(blocks[floor - 1][side][floor - 2], floor - 1, side, floor - 2);
+
+
+		if (side == 5) {			/// 마지막 꼭지점
+			v.push_back(blocks[floor][0][0]);
+			//out(blocks[floor][0][0], floor,  0, 0);
+		}
 	}
-	else {								// 변 가운데
-		v.push_back(blocks[floor][side][place - 1]);		// 이전 블록
-		v.push_back(blocks[floor - 1][side][place]);		// 아래층 블록, 아래층 블록 - 1
-		v.push_back(blocks[floor - 1][side][place - 1]);
+	else if (place == 0) {
+		//std::cout << "???????????????\n";
+		v.push_back(blocks[floor][side - 1][floor - 1]);
+
+		//out(blocks[floor][side - 1][floor - 1], floor, side - 1, floor - 1);
+
+		if (floor == 2) {
+			v.push_back(blocks[floor - 1][side - 1][place]);
+
+			//out(blocks[floor - 1][side - 1][place], floor - 1, side - 1, place);
+		}
+		else {
+			v.push_back(blocks[floor - 1][side - 1][floor - 2]);
+
+			//out(blocks[floor - 1][side - 1][floor - 2], floor - 1, side - 1, floor - 2);
+			//std::cout << "#################\n";
+		}
+
+		v.push_back(blocks[floor - 1][side][place]);
+
+		//out(blocks[floor - 1][side][place], floor - 1, side, place);
+	}
+	else {								/// 변 가운데
+		v.push_back(blocks[floor][side][place - 1]);		/// 이전 블록
+		v.push_back(blocks[floor - 1][side][place - 1]);	/// 아래층 블록 - 1
+		v.push_back(blocks[floor - 1][side][place]);		/// 아래층 블록
+
+		//out(blocks[floor][side][place - 1], floor, side, place - 1);
+		//out(blocks[floor - 1][side][place - 1], floor - 1, side, place - 1);
+		//out(blocks[floor - 1][side][place], floor - 1, side, place);
+		//std::cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
 	}
 
 	// int num = ((floor - 1) * 6) + (floor * side) + (place + 2);
 
-	std::cout << "Adjust nums : ";
-	for (int i : v) std::cout << i << ", ";
-	std::cout << '\n';
+	//std::cout << "Adjust nums : ";
+	//for (int i : v) std::cout << i << ", ";
+	//std::cout << '\n';
 	return v;
 }
 
+void init() {
+	blocks[0][0][0] = 1, count[1]++;
 
-void make() {
-	blocks[0][0][0] = 1;		//1
+	blocks[1][0][0] = 2, count[2]++;
+	blocks[1][1][0] = 3, count[3]++;
+	blocks[1][2][0] = 4, count[4]++;
+	blocks[1][3][0] = 5, count[5]++;
+	blocks[1][4][0] = 2, count[2]++;
+	blocks[1][5][0] = 3, count[3]++;
 
-	blocks[1][0][0] = 2;		//2
-	blocks[1][1][0] = 3;		//3
-	blocks[1][2][0] = 4;		//4
-	blocks[1][3][0] = 5;		//5
-	blocks[1][4][0] = 2;		//6
-	blocks[1][5][0] = 3;		//7
-	FE(i, 1, 6) count[i]++;
-
+	int num = 8;
 	int floor = 2;
-	while (floor < 180) {
+	while (floor < MAX) {
 		F(side, 6) {
 			F(place, floor) {
+				//std::cout << "\n\nNUM : " << num++ << "   BLOCK : " << floor << ", " << side << ", " << place << '\n';
 				vi v = check_adjust_block(floor, side, place);
-
 				int new_block = min_count_block(v);
-				std::cout << new_block << '\n';
-				blocks[floor][side][place] = new_block;
 
+				blocks[floor][side][place] = new_block;
 				count[new_block]++;
+				//std::cout << "ADD : " << new_block << '\n';
 			}
 		}
 		floor++;
@@ -118,16 +177,24 @@ void make() {
 }
 
 int main() {
-	FIO;
-	make();
-
+	FIO; init();
 	int n; std::cin >> n;
 	while (n--) {
 		int num; std::cin >> num;
-
 		std::cout << play(num) << '\n';
-		std::cout << "\n\n";
 	}
-
 	return 0;
 }
+
+/*
+4
+1
+4
+10
+100
+
+1
+4
+5
+5
+*/
