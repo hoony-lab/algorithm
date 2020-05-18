a242f5a41bb97855986ccf7acb1150ce3af06886
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-05-19 01:10:03 +0900

    Update boj 3649 로봇 프로젝트.cpp
    infinite input without declaring limit
    ???

--- Algorithm/boj 3649 로봇 프로젝트.cpp
+++ Algorithm/boj 3649 로봇 프로젝트.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define NANO 10'000'000
-using vi = std::vector<int>;	vi v, v_match;
-struct Ans { int s = 1e9, e = 1e9; } ans;
+//#define NANO 10'000'000
+using vi = std::vector<int>;
 int main() {
-	int x, t;	std::cin >> x >> t;		x *= NANO;
+	std::ios_base::sync_with_stdio(false); std::cin.tie(NULL);
 
-	while (t--) {
-		int n;	std::cin >> n;	v.push_back(n);
+	int x;
+	while (scanf_s("%d", &x) == 1) {
+		vi v;
+		x *= 10000000;
+
+		bool done = false;
+		int t; std::cin >> t; 
+		while (t--) {
+			int n;	std::cin >> n;	v.push_back(n);
+		}
+
+		std::sort(v.begin(), v.end());
+
+		for (int i = 0; i < v.size() - 1; ++i) {
+			int match = std::upper_bound(v.begin(), v.end(), x - v[i]) - v.begin() - 1;
+
+			if (v[match] + v[i] != x) continue;
+
+			std::cout << "yes " << v[i] << " " << v[match] << '\n';
+			done = true; 
+			break;
+		}
+		if (done) continue;
+		std::cout << "danger\n";
 	}
-	
-	std::sort(v.begin(), v.end());
-
-	for (int i = 0; i < v.size() - 1; ++i) {
-		int match = std::upper_bound(v.begin(), v.end(), x - v[i]) - v.begin() - 1;
-
-		if (v[match] + v[i] != x) continue;
-		
-		//std::cout << match << " : " << v[match] << "\n";
-
-		if (abs(ans.e - ans.s) < abs(v[match] - v[i]))
-			ans.e = v[match], ans.s = v[i];
-	}
-
-	if(ans.s == 1e9)	std::cout << "danger ";
-	else				std::cout << "yes "<< ans.s << " " << ans.e;
-
 	return 0;
 }
