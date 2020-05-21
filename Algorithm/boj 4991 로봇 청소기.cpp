8c1c2fd55f2dcba3ee51e27eede771020a208bb8
hoony.kim <jihun9838@naver.com>
2020-05-21 11:50:34 +0900

    Update boj 4991 로봇 청소기.cpp
    bfs - calculate all dists in advance
    dfs - find shortest dist sum

--- Algorithm/boj 4991 로봇 청소기.cpp
+++ Algorithm/boj 4991 로봇 청소기.cpp
@@ -16,23 +16,23 @@ bool operator<(const P p1, const P p2) { return p1.x < p2.x; }
 using vP = std::vector < P >;
 int dx[] = { 0, 1, 0, -1 }, dy[] = { 1,0,-1,0 };
 
+int dist[MAX][MAX];
 char map[MAX][MAX];
 bool visit_trash[MAX];
 int m, n, ans;
 vP v;
 std::set<P> st;
 
-
 void print(char visit[][MAX]) {
 	F(x, n) { F(y, m) { if (map[x][y] == 'x') std::cout << "X "; else std::cout << visit[x][y] << " "; }std::cout << '\n'; }std::cout << "\n\n";
 }
 void print(int visit[][MAX]) {
-	F(x, n) { F(y, m) { if(map[x][y] == 'x') std::cout << "X "; else std::cout << visit[x][y] << " "; }std::cout << '\n'; }std::cout << "\n\n";
+	F(x, n) { F(y, m) { if (map[x][y] == 'x') std::cout << "X "; else std::cout << visit[x][y] << " "; }std::cout << '\n'; }std::cout << "\n\n";
 }
 
-int bfs(P sp, int trash_num) {
-	int sx = sp.x, sy = sp.y;
-	int ex = v[trash_num].x, ey = v[trash_num].y;
+int bfs(int s, int e) {
+	int sx = v[s].x, sy = v[s].y;
+	int ex = v[e].x, ey = v[e].y;
 
 	int visit[MAX][MAX]{};	visit[sx][sy] = 1;
 	std::queue<P> q;		q.push({ sx, sy });
@@ -49,7 +49,6 @@ int bfs(P sp, int trash_num) {
 
 			if (nx == ex && ny == ey) {
 				//std::cout << sx << ", " << sy << " ::: " << ex << ", " << ey <<  " ::: " << visit[nx][ny] - 1 << '\n';
-				
 				return visit[nx][ny] - 1;
 			}
 		}
@@ -57,22 +56,19 @@ int bfs(P sp, int trash_num) {
 	return 1e5;
 }
 
-void dfs(P sp, bool visit_trash[], char map[][MAX], int time, int depth) {
+void dfs(int sum, int depth, int start, bool visit_trash[]) {
 
-	if (depth == v.size()) {
-		//std::cout << ans << ", " << time << '\n';
-		if (ans > time) ans = time;
+	if (depth == v.size() - 1) {
+		//std::cout << ans << ", " << sum << '\n';
+		if (ans > sum) ans = sum;
 	}
 
-	F(trash_num, v.size()) {
-		if (visit_trash[trash_num]) continue;
+	F(i, v.size()) {
+		if (visit_trash[i] || dist[start][i] == 1e5) continue;
 
-		char cmap[MAX][MAX];	memcpy(cmap, map, sizeof(map));		
-		int find_time = bfs(sp, trash_num);
-
-		visit_trash[trash_num] = true;
-		dfs(v[trash_num], visit_trash, cmap, time + find_time, depth + 1);
-		visit_trash[trash_num] = false;
+		visit_trash[i] = true;
+		dfs(sum + dist[start][i], depth + 1, i, visit_trash);
+		visit_trash[i] = false;
 	}
 }
 
@@ -83,8 +79,8 @@ int main() {
 	while (true) {
 		memset(visit_trash, 0, sizeof(visit_trash));
 		memset(map, 0, sizeof(map));
-		v.clear(); 
-		ans = 1e9;
+		v.clear();
+		ans = 1e5;
 
 		std::cin >> m >> n; if (!m) break;
 		F(x, n) F(y, m) {
@@ -92,18 +88,22 @@ int main() {
 			if (map[x][y] == '*') v.push_back({ x, y });
 			else if (map[x][y] == 'o') sp.x = x, sp.y = y;
 		}
+		v.push_back(sp);
 
-		char cmap[MAX][MAX];	memcpy(cmap, map, sizeof(map));
-		dfs(sp, visit_trash, cmap, 0, 0);
+		F(i, v.size()) {
+			F(j, v.size()) {
+				if (j <= i) continue;
+				int d = bfs(i, j);
+				dist[i][j] = dist[j][i] = d;
+				//std::cout << i << ", " << j << " :: " << d << '\n';
+			}
+		}
+
+		visit_trash[v.size() - 1] = true;
+		dfs(0, 0, v.size() - 1, visit_trash);
 
 		if (ans == 1e5) std::cout << -1 << '\n';
 		else			std::cout << ans << '\n';
-
-		//bool fail = false;
-		//F(x, n) F(y, m) if (map[x][y] == '*') fail = true;
-		//	
-		//if(fail)	std::cout << -1 << '\n';
-		//else		std::cout << time << '\n';
 	}
 	return 0;
 }
