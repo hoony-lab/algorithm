92ac0cb88ce96a29354f9b06f8f359dc264f35e2
hoony.kim <jihun9838@naver.com>
2020-05-21 14:32:24 +0900

    Create boj 15685 드래곤 커브.cpp
    ???

---
+++ Algorithm/boj 15685 드래곤 커브.cpp
@@ -0,0 +1,26 @@
+//20200521 1419
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+#define F(i, n) for(int i = 0 ; i < n ; ++i)
+#define MAX 101
+// R U L D
+int dx[] = { 0,-1,0,1 }, dy[] = { 1,0,-1,0 };
+
+int map[MAX][MAX];
+int n, x, y, d, g;
+
+bool check_square(int x, int y) {
+	
+}
+int main() {
+	// x,y 드래곤 커브 시작점, d 방향, g 세대
+	int t; cin >> t;
+	while (t--) {
+		cin >> n >> x >> y >> d >> g;
+	}
+	
+
+	return 0;
+}
