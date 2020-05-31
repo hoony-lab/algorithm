63738876ab8c1b25572b70cb5e4c997fffa09fe6
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-05-31 22:59:53 +0900

    Update boj 15683 감시.cpp
    !!!helphelphelp

--- Algorithm/boj 15683 감시.cpp
+++ Algorithm/boj 15683 감시.cpp
@@ -12,7 +12,13 @@ using vP = std::vector<P>;
 using vi = std::vector<int>;
 vP cctv, wall;
 
+// R 
 int dx[] = { 0, -1,0, 1 }, dy[] = { 1,0,-1,0 };
+int way[][6] = { {},
+	{0, 1, 2, 3},
+	{},
+
+};
 int map[MAX][MAX], cmap[MAX][MAX];
 int cnt[MAX][4];
 int visit[MAX][MAX][MAX][4];
@@ -20,6 +26,12 @@ int n, m, ans = 1e9;
 
 bool check(int x, int y) {
 	return !(x < 0 || y < 0 || x >= n || y >= m || map[x][y] == 6);
+}
+void search(int x, int y, int w, bool chk) {
+	F(x, n) F(y, m) {
+		chk ? map[x][y] = 1 : map[x][y] = 0;
+
+	}
 }
 
 void dfs(int depth) {
@@ -31,12 +43,12 @@ void dfs(int depth) {
 	}
 
 	
+	int x = cctv[i].x, y = cctv[i].y;
 	F(i, (int)cctv.size()) {
-		cctv[i].x, cctv[i].y;
 		
-		search();
+		search(x, y, , true);
 		dfs(depth + 1);
-		search();
+		search(x, y, , false);
 	}
 }
 int main() {
@@ -108,3 +120,79 @@ int main() {
 	std::cout << n * m - (ans + (int)cctv.size() + (int)wall.size()) << '\n';
 	return 0;
 }
+
+
+/*
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+struct cctv {
+	int x, y, id;
+};
+
+int n, m, ans=1e9;
+int a[10][10], b[10][10];
+vector<cctv> v;
+const int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
+const int U = 1, R = 1<<1, D = 1<<2, L = 1<<3;
+vector<int> direct[6] = {{},
+	{U, R, D, L}, // CCTV 1 : One direction
+	{U|D, R|L},   // CCTV 2 : Two direction (180 degree)
+	{U|R, R|D, D|L, L|U},         // CCTV 3 : Two direction (90 degree)
+	{L|U|R, U|R|D, R|D|L, D|L|U}, // CCTV 4 : Three direction
+	{U|R|D|L}};   // CCTV 5 : Four direction
+
+void observe(int x, int y, int i, int d) {
+	// CCTV observation. d==1(On), d==-1(off)
+	for (int k=0; k<4; k++) {
+		if (i & (1<<k)) {
+			int nx = x, ny = y;
+			while (a[nx][ny] != 6) {
+				b[nx][ny] += d;
+				nx += dx[k], ny += dy[k];
+			}
+		}
+	}
+}
+
+void solve(int index) {
+	if (index == (int)v.size()) {
+		// Count blind areas.
+		int area = 0;
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=m; j++) {
+				if (b[i][j] == 0) area += 1;
+			}
+		}
+		if (ans > area) ans = area;
+		return;
+	}
+	int x = v[index].x, y = v[index].y, id = v[index].id;
+	for (auto i : direct[id]) {
+		observe(x, y, i, 1);  // Turn on cctv.
+		solve(index+1);
+		observe(x, y, i, -1); // Turn off cctv.
+	}
+}
+
+int main() {
+	scanf("%d %d", &n, &m);
+	for (int i=0; i<n+2; i++) {
+		for (int j=0; j<m+2; j++) {
+			if (i == 0 || i == n+1 || j == 0 || j == m+1) a[i][j] = 6;
+			else {
+				scanf("%d", &a[i][j]);
+				if (a[i][j] == 6) b[i][j] = 1;
+				else if (a[i][j] != 0) v.push_back({i, j, a[i][j]});
+			}
+		}
+	}
+	solve(0);
+	printf("%d\n", ans);
+	return 0;
+}
+
+
+ÃâÃ³: https://rebas.kr/732 [PROJECT REBAS]
+*/
