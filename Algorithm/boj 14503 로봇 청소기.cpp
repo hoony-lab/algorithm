7b9d0bc72265963878422130278cc49752a39f10
Hoony <44036052+jihun9838@users.noreply.github.com>
2020-06-01 18:21:35 +0900

    Update boj 14503 로봇 청소기.cpp

--- Algorithm/boj 14503 로봇 청소기.cpp
+++ Algorithm/boj 14503 로봇 청소기.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #define F(i, n) for(int i = 0 ; i < n ; ++i)
 #define MAX 51
+#define LEFT 1
+#define BACK 2
 
 // U R D L
 // wall 1, empty 0
@@ -9,22 +11,41 @@ int dx[] = { -1,0,1,0 }, dy[] = { 0,1,0,-1 };
 struct p { int x, y; };
 int n, m, r, c, d;
 int map[MAX][MAX];
+
 bool check(int x, int y) {
 	return !(/*x <= 0 || y <= 0 || x >= n - 1 || y >= m - 1 ||*/ map[x][y] == 1);
 }
 
-p turn(int x, int y, int way) {
-	return { (x + 3 + dx[way]) % 4, (y + 3 + dy[way]) % 4 };
+p turn(int x, int y, int way, int turn){
+	return {x + dx[(4 + way - turn) % 4] , y + dy[(4 + way - turn) % 4] };
 }
 
+void play() {
+	int tx = r, ty = c, way = d;
+	while (1) {
+		bool done = false;
+
+		F(a, 4) {
+			//int nx = tx + dx[a], ny = ty + dy[a];
+			p npos = turn(tx, ty, d, LEFT + a);
+
+			if (map[npos.x][npos.y]) continue;
+			
+			map[npos.x][npos.y] = -1;
+
+			d = (4 + d - (LEFT + a));
+			
+			
+			break;
+
+		}
+		if (done) return;
+	}
+}
 int main() {
 	std::cin >> n >> m >> r >> c >> d;
 
-	int tx = r, ty = c;
-	F(a, 4) {
-		int nx = tx + dx[a], ny = ty + dy[a];
-		
-	}
+	play();
 
 	return 0;
 }
