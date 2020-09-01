3e383da44ac2e4e12cc05a3a791c6cc5765970b0
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-09-01 23:37:17 +0900

    Update boj 2116 주사위 쌓기.cpp

--- Algorithm/boj 2116 주사위 쌓기.cpp
+++ Algorithm/boj 2116 주사위 쌓기.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 #define F(i, n) for(int i = 0 ; i < n ; ++i)
+#define MAX(a, b) a > b ? a : b
 using namespace std;
 
 int dt[][6] = {
@@ -12,23 +11,42 @@ int dt[][6] = {
 	{4,1,0,3,5,2},
 	{5,1,4,3,2,0}
 };
-int n, dice[10001][6], ans;
+int n, dice[10001][6], ans, sum;
 
-void solve(int cnt, int dice) {
-	if (cnt == n) {
-					
+void solve(int lvl, int surface) {
+	cout << "LVL : " << lvl << " :: SURFACE : " << surface << '\n';
+	if (lvl == n) {
+		ans = MAX(ans, sum);
+		cout << "ANS : " << ans << "\n\n";
+		return;
+	}
+
+	F(top, 6) {
+		// pass whats not having same surface  / dice[lvl][surface]
+		if (surface != dice[lvl+1][dt[top][5]]) continue;
+		cout << surface << ":" << dice[lvl + 1][dt[top][5]] << " > " << dice[lvl + 1][dt[top][0]] << '\n';
+
+		int tmp = 0;
+		F(side, 5) {
+			// pass Top & Bottom
+			if (!side) continue;
+
+			// max side num
+			tmp = MAX(tmp, dice[lvl][dt[top][side]]);
+			cout << "TOP : " << top << ", SIDE : " << side << ", TMP : " << tmp << '\n';
+		}
+		sum += tmp;
+		solve(lvl + 1, dice[lvl][top]);
+		sum -= tmp;
 	}
 }
 
 int main() {
 	cin >> n; F(i, n) F(j, 6) cin >> dice[i][j];
 
-	
+	// check all surfaces
 	F(i, 6) solve(0, dice[0][i]);
 	
-	F(i, 5) { if (!i) continue;
-		
-	}
-
+	cout << ans;
 	return 0;
 }
