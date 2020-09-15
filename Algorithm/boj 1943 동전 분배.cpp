d2e6a724f1e5a6072dcf87bc1671b06820bc20ea
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-09-15 10:25:23 +0900

    Update boj 1943 동전 분배.cpp

--- Algorithm/boj 1943 동전 분배.cpp
+++ Algorithm/boj 1943 동전 분배.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 struct money {
 	int p, c;
@@ -7,22 +8,52 @@ struct money {
 
 int main() {
 	for (int t = 0; t < 3; ++t) {
-		int n, tp, tc, sum = 0;
+		int n, tp, tc, total = 0, sum[2]{};
 		vector<money> v;
 
 		cin >> n;
 		for (int i = 0; i < n; ++i) {
 			cin >> tp >> tc;
 			v.push_back({ tp, tc });
-			sum += tp * tc;
+			total += tp * tc;
 		}
 
-		if (sum % 2 == 1) {
+		if (total % 2 == 1) {
 			cout << 0 << '\n';
 			continue;
 		}
 
+		sort(v.begin(), v.end(), [](const money& v1, const money& v2) {
+			return v1.p > v2.p;
+		});
 
+
+
+		int dp[100000]{};
+		for (int i = 1; i <= total; i++) dp[i] = 1e9;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= total; j++) {
+				if (dp[j] == 1e9) continue;
+
+				if ((dp[j] < v[i].c) && (j + v[i].p <= total)) {
+					if (dp[j + v[i].p] > dp[j] + 1)
+						dp[j + v[i].p] = dp[j] + 1;
+				}
+				dp[j] = 0;
+			}
+		}
+		if (dp[total / 2] != 1e9)	cout << 1 << '\n';
+		else						cout << 0 << '\n';
+
+		//for (int i = 0; i < v.size(); i++) {
+		//	for (int cc = 0; cc < v[i].c; cc++) {
+		//		if (sum[0] < sum[1])	sum[0] += v[i].p;
+		//		else					sum[1] += v[i].p;
+		//	}
+		//}
+		//if (sum[0] == sum[1])	cout << 1 << '\n';
+		//else					cout << 0 << '\n';
 	}
 	return 0;
 }
