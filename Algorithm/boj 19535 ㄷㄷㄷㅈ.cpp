2be0964cd626dd1fc1e6ad3c03f4ce147c9ccd9f
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-09-16 20:39:44 +0900

    Update boj 19535 ㄷㄷㄷㅈ.cpp
    graph, tree, math

--- Algorithm/boj 19535 ㄷㄷㄷㅈ.cpp
+++ Algorithm/boj 19535 ㄷㄷㄷㅈ.cpp
@@ -2,33 +2,52 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
+#define ll long long
 #define MAX 300001
 #define D cout << "D"
 #define G cout << "G"
 #define DG cout << "DUDUDUNGA"
-int find_d() {
+ll dd, gg;
+int visited[MAX];
 
-}
-int find_g() {
-
-}
 int main() {
     int n, u, v;
     cin >> n;
+    vector<vector<int>> graph(n+1);
 
     //vector<int> v(MAX); generate(v.begin(), v.end(), [num = 0]() mutable {return num++; });
 
-    vector<int> graph[MAX];
     for (int i = 1; i < n; ++i) {
         cin >> u >> v;
         graph[u].push_back(v);
         graph[v].push_back(u);
-        
-        find_g();
-        find_d();
     }
 
-	return 0;
+    //find_g();
+    //find_d();
+
+    for (int i = 1; i <= n; ++i) {
+        if (graph[i].size() >= 3)  {
+            ll cnt = graph[i].size();
+            gg += (cnt * (cnt - 1) * (cnt - 2)) / 6;
+        }
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        ll max = graph[i].size() - 1;
+        for (int j : graph[i]) {
+            if (visited[j]) continue;
+            dd += max * (graph[j].size() - 1);
+        }
+        visited[i] = true;
+    }
+
+
+    if (dd > gg * 3) D;
+    else if (dd < gg * 3) G;
+    else DG;
+
+    return 0;
 }
 
 /*
