7b1777bacd0369941943b1379e3e1bdd5c95105a
hooooony <jihun9838@gmail.com>
2020-10-06 13:07:41 +0900

    Create boj 1062 가르침.cpp

--- Algorithm/boj 1062 가르침.cpp
+++ Algorithm/boj 1062 가르침.cpp
@@ -6,23 +6,62 @@
 #include <map>
 using namespace std;
 #define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
-string prefix = "anta", suffix = "tica", tmp;
-map<char, int> mp;
+
 int ans;
+vector<string> v;
+
+int n, k, check[26];
+int max_cnt() {
+
+	return 1;
+}
+void dfs(int idx, int cnt) {
+	if (k < 5) return;
+
+	if (cnt == k - 5) {
+		int sum = 0;
+		for (int i = 0; i < n; ++i) {
+			int tmp_cnt = 0;
+
+			for (int j = 0; j < (int)v[i].size(); ++j) {
+				if (check[v[i][j] - 'a']) tmp_cnt++;
+				else break;
+			}
+
+			if (tmp_cnt == v[i].size())
+				sum++;
+		}
+
+		ans = max(ans, sum);
+		return;
+	}
+
+	for (int i = idx; i < 26; ++i) {
+		if (check[i]) continue;
+		check[i]++;
+		dfs(i, cnt + 1);
+		check[i]--;
+	}
+}
 int main() {
-	int n, k;
 	cin >> n >> k;
 
-	for (int i = 0; i < n; i++) {
-		cin >> tmp;
-		tmp = tmp.substr(4, tmp.length() - 8);
-		for (auto c : tmp) {
-			mp[c]++;
-		}
+	for (int i = 0; i < n; ++i) {
+		string str;
+		cin >> str;
+		v.push_back(str);
 	}
-	for (auto c : mp) {
-		cout << c.first << '\n';
-	}
-	cout << ans << '\n';
+
+	// string prefix = "anta", suffix = "tica";
+	// a n t i c
+	check[0]++;
+	check[2]++;
+	check[8]++;
+	check[13]++;
+	check[19]++;
+	
+	dfs(0, 0);
+
+	cout << ans;
 	return 0;
 }
