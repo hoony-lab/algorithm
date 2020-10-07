afce609a01fb98ff7991c4e0efefdad257204c7f
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-10-08 00:48:03 +0900

    Update boj 1114 통나무 자르기.cpp

--- Algorithm/boj 1114 통나무 자르기.cpp
+++ Algorithm/boj 1114 통나무 자르기.cpp
@@ -3,8 +3,8 @@
 #define ll long long
 using namespace std;
 
-int l, k, c, cut[10001], ans = 0;
-int s, e, m;
+int l, k, c, cut[10101], ans = 0, len[10101];
+int s, e, mid;
 
 //bool foo(int idx) {
 //	if (cut[idx - 1] > m) return false;
@@ -27,53 +27,110 @@ int s, e, m;
 //	return true;
 //}
 
-bool foo(int len, int chk) {
-	int p = l, cnt = 0;
-	for (int i = k; i >= 0; --i) {
-		if (cut[i + 1] - cut[i] > len) return false;
-		if (p - cut[i] > len) {
-			cnt++;
-			p = cut[i + 1];
+//bool foo(int len, int chk) {
+//	int p = l, cnt = 0;
+//	for (int i = k; i >= 0; --i) {
+//		if (cut[i + 1] - cut[i] > len) return false;
+//		if (p - cut[i] > len) {
+//			cnt++;
+//			p = cut[i + 1];
+//		}
+//	}
+//	if (cnt <= c) {
+//		if (chk)	return cut[1];		// 처음꺼 구하기
+//		else		return true;
+//	}
+//
+//	return false;
+//}
+//
+//int main() {
+//	cin >> l >> k >> c;
+//	for (int i = 1; i <= k; ++i) cin >> cut[i];
+//	cut[0] = 0, cut[k + 1] = l;
+//
+//	sort(cut + 1, cut + k + 1);
+//
+//	// 길이 기준
+//	s = 0, e = 1e9;
+//	while (s < e) {
+//		m = (s + e) / 2;	
+//
+//		if (foo(m, 0)) e = m;
+//		else s = m + 1;
+//	}
+//
+//	cout << e << " " << foo(e, 1);
+//	return 0;
+//
+//	//int d = 0;
+//	//for (int i = 1; i <= k; ++i) {
+//	//	if (foo(i + 1)) {
+//	//		d = i;
+//	//		break;
+//	//	}
+//	//}
+//
+//	//cout << ans << " " << cut[d];
+//	//return 0;
+//}
+
+int foo(int tmp) {
+	int last_size = 0;
+	int cut_cnt = 0;
+
+	for (int i = k; i >= 0; i--) {
+		if (len[i] > tmp) return -1;
+
+		if (last_size + len[i] > tmp) {
+			last_size = len[i];
+			cut_cnt++;
 		}
-	}
-	if (cnt <= c) {
-		if (chk)	return cut[1];		// 처음꺼 구하기
-		else		return true;
+		else last_size += len[i];
 	}
 
-	return false;
+	if (cut_cnt < c)		return len[0];
+	else if (cut_cnt == c)	return last_size;
+	else					return -1;
 }
 
 int main() {
 	cin >> l >> k >> c;
-	for (int i = 1; i <= k; ++i) cin >> cut[i];
-	cut[0] = 0, cut[k + 1] = l;
+	for (int i = 0; i < k; i++)
+		cin >> cut[i];
 
-	sort(cut + 1, cut + k + 1);
+	sort(cut, cut + k);
+	len[0] = cut[0];
 
-	// 길이 기준
-	s = 0, e = 1e9;
-	while (s < e) {
-		m = (s + e) / 2;	
-
-		if (foo(m, 0)) e = m;
-		else s = m + 1;
+	for (int i = 1; i < k; i++) {
+		len[i] = cut[i] - cut[i - 1];
 	}
+	len[k] = l - cut[k - 1];
 
-	cout << e << " " << foo(e, 1);
+	s = 1, e = l;
+
+	int ans = -1;
+	int ans2 = 0;
+
+	// 잘라야하는 길이 기준 이분탐색
+	while (s <= e) {
+		mid = (s + e) / 2;
+		int tmp = foo(mid);
+
+		if (tmp > 0) {
+			e = mid - 1;
+			ans = mid;
+			ans2 = tmp;
+		}
+		else {
+			s = mid + 1;
+		}
+	}
+	cout << ans << " " << ans2;
+
 	return 0;
-
-	//int d = 0;
-	//for (int i = 1; i <= k; ++i) {
-	//	if (foo(i + 1)) {
-	//		d = i;
-	//		break;
-	//	}
-	//}
-
-	//cout << ans << " " << cut[d];
-	//return 0;
 }
+
 
 /*
 9 2 1
