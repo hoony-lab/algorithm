309e6da63b647214f0080b9995c503bb13855d90
hooooony <jihun9838@gmail.com>
2020-10-08 13:49:50 +0900

    Update boj 19238 스타트 택시.cpp

--- Algorithm/boj 19238 스타트 택시.cpp
+++ Algorithm/boj 19238 스타트 택시.cpp
@@ -3,15 +3,14 @@
 #include <vector>
 using namespace std;
 #define F(i, n) for(int i = 0 ; i < n ; ++i)
-#define MAX 21
+#define MAX 22
 
 struct PASSENGER { int px, py, pxx, pyy, passenger_dest, passenger_taxi; };
 struct P { int x, y; };
 
 int dx[] = { -1,0,1,0 };
 int dy[] = { 0,1,0,-1 };
-int map[MAX][MAX];
-int n, m, fuel, taxi_x, taxi_y, px, py, pxx, pyy, ans, done[MAX], done_cnt;
+int n, m, fuel, taxi_x, taxi_y, px, py, pxx, pyy, map[MAX][MAX], ans, done[MAX * MAX], done_cnt;
 
 vector<PASSENGER> v;
 
@@ -20,10 +19,10 @@ bool check(int x, int y) {
 }
 
 // 승객 - 목적지, 택시 거리
-void cal_passenger_dest_taxi_dist() {
+int cal_passenger_dest_taxi_dist() {
 
 	F(i, (int)v.size()) {
-		if (done[i]) continue;
+		//if (done[i]) continue;
 
 		int x = v[i].px, y = v[i].py;
 		int visit[MAX][MAX]{};
@@ -34,7 +33,6 @@ void cal_passenger_dest_taxi_dist() {
 		// 일단 못가는 걸로 가정
 		v[i].passenger_taxi = -1;
 		v[i].passenger_dest = -1;
-
 
 		while (!q.empty()) {
 			int tx = q.front().x, ty = q.front().y;
@@ -57,9 +55,14 @@ void cal_passenger_dest_taxi_dist() {
 				visit[nx][ny] = visit[tx][ty] + 1;
 			}
 		}
-		cout << x << ", " << y << " : " << v[i].passenger_taxi << ", " << v[i].passenger_dest << '\n';
-	}
+		// 택시가 승객까지 못가거나 || 승객이 목적지까지 못가면
+		if (v[i].passenger_taxi == -1 || v[i].passenger_dest == -1) {
+			return 0;
+		}
 
+		//cout << x << ", " << y << " : " << v[i].passenger_taxi << ", " << v[i].passenger_dest << '\n';
+	}
+	return 1;
 }
 
 /// 택시 - 승객들 거리
@@ -67,15 +70,11 @@ void cal_passenger_dest_taxi_dist() {
 // 그중 행 번호가 가장 작은 승객을
 // 그중 열 번호가 가장 작은 승객을
 int min_taxi_passenger_dist_idx() {
-	int min_passenger_taxi_dist = 1e9;
-	int min_passenger_taxi_idx = MAX;
-	int invalid_cnt = 0;
+	int min_passenger_taxi_dist = MAX * MAX;
+	int min_passenger_taxi_idx = MAX * MAX;
 
 	F(i, (int)v.size()) {
-		if (done[i]) continue;
-		if (v[i].passenger_dest == -1) {
-			invalid_cnt++; continue;
-		}
+		//if (done[i]) continue;
 
 		if (min_passenger_taxi_dist > v[i].passenger_taxi) {
 			min_passenger_taxi_dist = v[i].passenger_taxi;
@@ -93,19 +92,20 @@ int min_taxi_passenger_dist_idx() {
 				min_passenger_taxi_idx = i;
 			}
 		}
+
 	}
 
-	if (invalid_cnt == m - done_cnt || 
-			v[min_passenger_taxi_idx].passenger_taxi > fuel ||
-			v[min_passenger_taxi_idx].passenger_dest + v[min_passenger_taxi_idx].passenger_taxi > fuel)
-		return MAX;
+	if (v[min_passenger_taxi_idx].passenger_dest + v[min_passenger_taxi_idx].passenger_taxi > fuel)
+		return MAX * MAX;
 	else {
-		done[min_passenger_taxi_idx] = true;
 		done_cnt++;
 		taxi_x = v[min_passenger_taxi_idx].pxx;
 		taxi_y = v[min_passenger_taxi_idx].pyy;
 
-		cout << "pick : " << min_passenger_taxi_idx << '\n';
+		//done[min_passenger_taxi_idx] = true;
+		
+		//cout << "pick : " << min_passenger_taxi_idx << '\n';
+
 		return min_passenger_taxi_idx;
 	}
 }
@@ -125,27 +125,32 @@ int main() {
 
 	init();
 
-	while (done_cnt != m) {
+	F(i, m) {
 		
-		cout << "done_cnt : " << done_cnt << " : start : " << taxi_x << ", " << taxi_y << " / " << fuel << '\n';
+		//cout << "done_cnt : " << done_cnt << " : start : " << taxi_x << ", " << taxi_y << " / " << fuel << '\n';
 
-		cal_passenger_dest_taxi_dist();
+		if (!cal_passenger_dest_taxi_dist()) {
+			cout << -1;
+			return 0;
+		}
 
 		int idx = min_taxi_passenger_dist_idx();
 
-		// 승객이 하나라도 목적지에 도달 못하면 || 남은 연료보다 거리가 멀면
-		if (idx == MAX) {
+		// 남은 연료보다 거리가 멀면
+		if (idx == MAX * MAX) {
 			cout << -1;
 			return 0;
 		}
 
 		// 연료는 한 칸 이동할 때마다 1만큼 소모
-		// 한 승객을 목적지로 성공적으로 이동 > 소모한 연료 양의 두 배가 충전
+		// 한 승객을 목적지로 성공적으로 이동 >> 목적지로 소모한 연료 양의 두 배가 충전
 		fuel = fuel - v[idx].passenger_taxi + v[idx].passenger_dest;
+		v.erase(v.begin() + idx);
 	}
 	
 	cout << fuel;
- 	return 0;
+
+	return 0;
 }
 /*
 6 3 100
@@ -192,18 +197,60 @@ int main() {
 
 >> 20
 
-//6 4 100
-//0 0 1 0 0 0
-//0 0 1 0 0 0
-//0 0 0 0 0 0
-//0 0 0 0 0 0
-//0 0 0 0 1 0
-//0 0 0 1 0 0
-//6 5
-//1 6 2 2		-6 +7
-//1 6 3 5		-7 +3
-//1 6 5 4		-3 +6
-//1 6 1 1		-6 +9
-//
-//>> -1
+6 3 10
+0 0 1 0 0 0
+0 0 1 0 0 0
+0 0 0 1 0 0
+0 0 0 0 0 0
+0 0 0 0 1 0
+0 0 0 1 0 0
+6 5
+5 6 4 6
+4 6 2 2
+2 2 1 1
+
+>> 17
+
+6 3 17
+0 0 1 0 0 0
+0 0 1 0 0 0
+0 0 0 1 0 0
+0 0 0 1 0 0
+0 0 0 0 1 0
+0 0 0 1 0 0
+6 5
+5 6 4 6
+2 2 1 1
+4 6 1 3
+
+>> -1
+
+// 거리가 같으니 행이 작은
+6 2 17
+0 0 1 0 0 0
+0 0 1 0 0 0
+0 0 0 1 0 0
+0 0 0 0 0 0
+0 0 0 0 1 0
+0 0 0 0 0 0
+6 6
+5 4 6 1
+4 5 3 3
+
+>> 18
+
+// 거리가 같고 행이 같으면 열이 작은
+6 3 17
+0 0 1 0 0 0
+0 0 1 0 0 0
+0 0 0 1 0 0
+0 0 0 0 0 0
+0 0 0 0 1 0
+0 0 0 0 0 0
+6 5
+4 4 6 1
+4 6 3 6
+5 3 1 1
+
+>> 15
 */
