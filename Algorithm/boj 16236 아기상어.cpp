e928a1a809c57bdaad28184a0fb5a31717301c67
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-10-09 18:36:06 +0900

    Update boj 16236 아기상어.cpp

--- Algorithm/boj 16236 아기상어.cpp
+++ Algorithm/boj 16236 아기상어.cpp
@@ -1,149 +1,3 @@
-//#include <iostream>
-//#include <vector>
-//#include <queue>
-//#include <algorithm>
-//#include <memory.h>
-//using namespace std;
-
-//int dy[] = { -1,0,0,1 };
-//int dx[] = { 0,-1,1,0 };
-//
-///*
-//
-//자신보다 작은 물고기를 먹음
-//먹을게없으면 엄마상어에게 도움
-//1마리보다 많으면 가장 가까운물고기 / 그중 위에있는 / 그중 왼쪽에있는
-//지나가야하는 칸의 최솟값
-//먹는데 시간은 안걸림 / 먹으면 빈칸
-//자신의크기 갯수만큼 먹어야 크기증가
-//
-//	for (int y = 0; y < N; ++y)	{
-//		for (int x = 0; x < N; ++x) {
-//
-//		}
-//	}
-//
-//
-//*/
-//int answer;
-//int N, shark, feed, hunt[8] = { 0, }, yy, xx;
-//int map[21][21], visit[21][21];
-//queue<pair<int, int>> q;
-//
-//void print() {
-//	for (int y = 0; y < N; ++y)	{
-//		for (int x = 0; x < N; ++x) {
-//			//cout << map[y][x] << " ";
-//			cout << visit[y][x] << " ";
-//		}
-//		cout << '\n';
-//	}
-//	cout << '\n';
-//}
-//
-//int main() {
-//
-//	ios_base::sync_with_stdio(false);
-//	cin.tie(NULL);
-//
-//	cin >> N;
-//	answer = 0;
-//	shark = feed = 2;
-//	memset(visit, 0, sizeof(visit));
-//
-//	for (int y = 0; y < N; y++) {
-//		for (int x = 0; x < N; x++) {
-//			cin >> map[y][x];
-//			if (map[y][x] == 9) {
-//				q.push({ y,x });
-//				visit[y][x] = 1;
-//				map[y][x] = 0;
-//			}
-//			hunt[map[y][x]]++;
-//		}
-//	}
-//
-//	if (hunt[1] == 0){
-//		cout << answer; 
-//
-//		return answer;
-//	}
-//
-//	bool no_more_hunt = false;
-//
-//	while (!q.empty()) {
-//		int ty = q.front().first;
-//		int tx = q.front().second;
-//		q.pop();
-//		cout << "CHEKKING " << ty << ", " << tx << '\n';
-//
-//		for (int a = 0; a < 4; a++){
-//			int ny = ty + dy[a];
-//			int nx = tx + dx[a];
-//
-//			if (ny < 0 || nx < 0 || ny >= N || nx >= N || visit[ny][nx] > 0 || map[ny][nx] > shark) continue;
-//			cout << ny << " ," << nx << '\n';
-//
-//			if (map[ny][nx] == 0 || map[ny][nx] == shark) {
-//				q.push({ ny,nx });
-//				visit[ny][nx] = visit[ty][tx] + 1;
-//			}
-//			//상어보다 몸집이 작으면 냠냠 하고 재탐색
-//			else if (map[ny][nx] < shark) {
-//				while(!q.empty())	q.pop();
-//
-//				print();
-//				cout << q.size() << ", " << ny << ", " << nx << '\n';
-//
-//				answer += visit[ty][tx];
-//				
-//				//hunt[map[ny][nx]]--;
-//				map[ny][nx] = 0;
-//
-//				//더이상 먹을게 없을때 중간 가지치기
-//				/*
-//				for (int i = 1; i < shark; i++)
-//				{
-//					if(hunt[i] < feed) no_more_hunt = true;
-//				}
-//				*/
-//
-//
-//				q.push({ ny,nx });
-//				memset(visit, 0, sizeof(visit));
-//				visit[ny][nx] = 1;
-//
-//				//자기 몸집만큼 먹으면 상어 성장
-//				if (--feed == 0) feed = ++shark;
-//				break;
-//			}
-//
-//
-//		}
-//
-//
-//		if (no_more_hunt) break;
-//		else {
-//
-//		}
-//
-//	}
-//
-//	cout << answer;
-//
-//	
-//
-//	return answer;
-//
-//	//for (int y = 0; y < N; ++y)	{
-//	//	for (int x = 0; x < N; ++x) {
-//	//		if (map[y][x] < shark)
-//	//			
-//	//	}
-//	//}
-//
-//}
-
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -156,10 +10,9 @@ struct FISH { int x, y, dist; };
 int dx[] = { -1,0,1,0 };
 int dy[] = { 0,1,0,-1};
 
-int ans;
+// 가장 처음에 아기 상어의 크기는 2
+int baby_size = 2, feed, yy, xx, ans;
 int n, map[MAX][MAX], visit[MAX][MAX];
-int baby_size = 2, feed, yy, xx;
-queue<pair<int, int>> q;
 vector<FISH> v;
 bool check(int x, int y) {
 	return !(x < 0 || y < 0 || x >= n || y >= n);
@@ -177,38 +30,93 @@ bool comp(FISH f1, FISH f2) {
 	return false;
 }
 
+void eat_fish() {
+	// 먹을 수 있는 물고기가 1마리보다 많다면, 거리가 가장 가까운 물고기를 먹으러 간다.
+	//	거리는 아기 상어가 있는 칸에서 물고기가 있는 칸으로 이동할 때, 지나야하는 칸의 개수의 최솟값이다.
+	//	거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기, 그러한 물고기가 여러마리라면, 가장 왼쪽에 있는 물고기를 먹는다.
+	sort(v.begin(), v.end(), comp);
+	// 지도에 먹음 표시
+	map[v.front().x][v.front().y] = 0;
+	// 생선까지 걸린 시간
+	ans += v.front().dist;
+	// 아기 상어가 먹을 수 있는 물고기가 있는 칸으로 이동
+	xx = v.front().x, yy = v.front().y;
+
+	//cout << "ERASE_FISH : " << v.front().x << ", " << v.front().y << " : " << v.front().dist << " , ANS : " << ans << '\n';
+	// 물고기를 먹으면, 그 칸은 빈 칸이 된다.
+	v.erase(v.begin());
+
+	// 아기 상어는 자신의 크기와 같은 수의 물고기를 먹을 때 마다 크기가 1 증가
+	if (++feed == baby_size) {
+		baby_size++;
+		//cout << "SIZE UP SIZE UP SIZE UP: " << baby_size << '\n';
+		// 경험치 초기화
+		feed = 0;
+	}
+}
+
 void baby_to_fish_dist() {
+	v.clear();
+
 	int visit[MAX][MAX]{};
 	queue<FISH> q;
-	q.push({ xx, yy });
+	q.push({ xx, yy, 1 });
 	visit[xx][yy] = 1;
 
+	int find_first_fish_dist = MAX * MAX;
 	while (!q.empty()) {
-		int tx = q.front().x, ty = q.front().y;
+		int tx = q.front().x, ty = q.front().y, tdist = q.front().dist;
 		q.pop();
-
-		F(i, (int)v.size()) 
-			if (v[i].x == tx && v[i].y == ty) 
-				v[i].dist = visit[tx][ty] - 1;
-		
+		//if (find_first_fish_dist > tdist) continue;
+		//cout << "search : " << tx << ", " << ty << " / " << tdist << '\n';
 		F(a, 4) {
 			int nx = tx + dx[a], ny = ty + dy[a];
-			if (!check(nx, ny) || baby_size < map[nx][ny]) continue;
-			q.push({ nx, ny, });
+		
+			if (!check(nx, ny) || baby_size < map[nx][ny] || visit[nx][ny] || find_first_fish_dist < visit[tx][ty]) continue;
+
+			q.push({ nx, ny, visit[tx][ty] });
 			visit[nx][ny] = visit[tx][ty] + 1;
+
+			//cout << "!!PUSH!! : " << nx << ", " << ny << '\n';
+
+			// 거리가 같은물고기
+			if (map[nx][ny] != 0 && baby_size > map[nx][ny]) {
+				v.push_back({ nx, ny, visit[nx][ny] - 1 });
+				find_first_fish_dist = visit[nx][ny] - 1;
+			} 
 		}
 	}
-	
 }
 
 int main() {
-	cin >> n; 
+	cin >> n;
 	F(x, n) F(y, n) {
 		cin >> map[x][y];
-		if (map[x][y] && map[x][y] != 9) v.push_back({ x,y,-1 });
-		if (map[x][y] == 9) xx = x, yy = y;
+		if (map[x][y] && map[x][y] != 9) v.push_back({ x,y, -1 });
+		if (map[x][y] == 9) xx = x, yy = y, map[x][y] = 0;
 	}
 
-	if (v.size() == 0) return 0;
+	while(1){
+		baby_to_fish_dist();
+
+		if (v.empty()) break;
+		//cout << "SAME_FISH_CNT : " << v.size() << " / FISH_DIST : " <<  v.front().dist << '\n';
+		//for (FISH f : v)cout << f.x << ", " << f.y << " : " << f.dist << '\n';
+		
+		eat_fish();
+	}
+
+	cout << ans;
 	return 0;
 }
+/*
+6
+5 4 3 2 3 4
+4 3 2 3 4 5
+3 2 9 5 6 6
+2 0 0 3 4 5
+3 2 0 6 5 4
+6 6 6 6 6 6
+
+>> 60
+*/
