b58c585fd7a50c72e0084fa59b8564f7f77c931d
Hoony <jihun9838@gmail.com>
2021-07-27 00:07:14 +0900

    Create boj 2003 수들의 합 2.cpp
    two pointer

---
+++ Algorithm/boj 2003 수들의 합 2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#define MAX 10001
+using namespace std;
+
+int num[MAX];
+
+int main() {
+    int n, m; cin >> n >> m;
+    for (int i = 0; i < n; ++i) cin >> num[i + 1];
+    
+    int start = 1, end = start, sum = num[start], ans = 0;
+
+	while (end <= n) {
+        // cout << "start : " << start << ", " << end << '\n';
+
+        if (sum < m) sum += num[++end];
+        else if (sum >= m) sum -= num[start++];
+        else if (sum == m) {
+            // cout << "\tfind : " << start << ", " << end << '\n';
+            ans++, sum -= num[start++];
+        }
+	}
+    
+    cout << ans;
+    return 0;
+}
