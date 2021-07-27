e0c4613b466c12f1610c68505ae792f57ba4fc76
Hoony <jihun9838@gmail.com>
2021-07-28 00:29:09 +0900

    Update boj 2003 수들의 합 2.cpp

--- Algorithm/boj 2003 수들의 합 2.cpp
+++ Algorithm/boj 2003 수들의 합 2.cpp
@@ -8,20 +8,19 @@ int num[MAX];
 int main() {
     int n, m; cin >> n >> m;
     for (int i = 0; i < n; ++i) cin >> num[i + 1];
-    
+    st
     int start = 1, end = start, sum = num[start], ans = 0;
 
-	while (end <= n) {
-        // cout << "start : " << start << ", " << end << '\n';
+    while (end <= n) {
+        cout << "start : " << start << ", " << end << '\n';
 
-        if (sum < m) sum += num[++end];
-        else if (sum >= m) sum -= num[start++];
+        if (sum < m)        sum += num[++end];
+        else if (sum > m)   sum -= num[start++];
         else if (sum == m) {
-            // cout << "\tfind : " << start << ", " << end << '\n';
             ans++, sum -= num[start++];
         }
-	}
-    
+    }
+
     cout << ans;
     return 0;
 }
